5eda866095e3767bc3aa3df67e019bb788d1be56
Chikaovsky <princenico46@gmail.com>
2022-10-04 13:47:28 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *  *_strlen - count array
+ *
+ *   *@s: array of elements
+ *
+ *    *Return: 1
+ *
+ *     */
+
+
+
+int _strlen(char *s)
+
+{
+
+		unsigned int i;
+
+
