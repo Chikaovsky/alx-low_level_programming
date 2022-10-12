b6feb95db78b8483830ea2dcfa5754f0d888e0c7
Princenico <111049274+Chikaovsky@users.noreply.github.com>
2022-10-12 21:33:39 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,53 +1,70 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
- * op_add - add two integer
- * @a: first integer
- * @b: second integer
- * Return: sum
- */
+  *op_add - return the sum of 2 numbers.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: sum.
+  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - substract two integer
- * @a: first integer
- * @b: second integer
- * Return: substraction
- */
+  *op_sub - returns difference of 2 numbers
+  *@a:  integer.
+  *@b: integer.
+  *
+  *Return: difference.
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - multiply two integer
- * @a: first integer
- * @b: second integer
- * Return: multiplication
- */
+  *op_mul - returns product of two numbers.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: product.
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - divide two integer
- * @a: first integer
- * @b: second integer
- * Return: division
- */
+  *op_div - returns the quotient of 2 numbers.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: quotient.
+  */
 int op_div(int a, int b)
 {
-		return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
+
 /**
- * op_mod - modulo of two integer
- * @a: first integer
- * @b: second integer
- * Return: modulo
- */
+  *op_mod - returns the remainder of division.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: remainder.
+  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
