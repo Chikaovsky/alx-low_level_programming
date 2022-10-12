dba44e0d9175b985095951a8584764062ed016e7
Chikaovsky <princenico46@gmail.com>
2022-10-12 12:29:49 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "function_pointers.h"
+
+/**
+ *
+ *   * print_name - print name
+ *
+ *     * @name: pointer to a char
+ *
+ *       * @f: pointer to a funtion
+ *
+ *         */
+
+
+
+void print_name(char *name, void (*f)(char *))
+
+{
+
+		if (name != NULL && f != NULL)
+
+				f(name);
+
+}
