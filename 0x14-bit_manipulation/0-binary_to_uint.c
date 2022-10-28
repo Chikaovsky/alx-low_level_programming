d4d1c3cceb62753b8b048991eec81d78dc48f7da
Princenico <111049274+Chikaovsky@users.noreply.github.com>
2022-10-28 23:29:47 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,37 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * binary_to_uint - converts binary to decimal
- * @b: string of binary number
- * Return: the converted number, or 0 if not.
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-int base = 1, i = 0, length = 0;
-unsigned int result = 0;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	while (b[length] != '\0')
-	{
-		length++;
-	}
-	for (i = length - 1; i >= 0; i--)
-	{
-		if (b[i] == '0' || b[i] == '1')
-		{
-			if (b[i] == '1')
-			{
-				result += base;
-			}
-			base = base * 2;
-		}
-		else
-			return (0);
 
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-		return (result);
+
+	return (ui);
 }
